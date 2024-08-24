e240570836b103e9ef20b76ec0d5b678d0166880
Goldside543 <goldside000@outlook.com>
2024-08-24 10:48:45 -0400

    Create Netcraft.java

---
+++ src/main/java/netcraft/Netcraft.java
@@ -0,0 +1,39 @@
+package netcraft;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+public class Netcraft extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Netcraft app = new Netcraft();
+        app.setShowSettings(false);
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        // This method will be called every game tick and can be used to make updates
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        // Render code here
+    }
+}
